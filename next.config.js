6f11a367f87f372526cbc4f78e2b8989b502bd9b
Adbspec <abhishekr0709@gmail.com>
2026-05-28 13:16:28 +0530

    Add wrangler toml

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  output: "standalone",
 };
 
 export default nextConfig;
